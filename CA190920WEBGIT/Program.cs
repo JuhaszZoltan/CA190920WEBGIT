7e5610e42a04185fa7471cf2d614f425f891e9f0
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2019-09-20 16:39:54 +0200

    Add project files.

---
+++ CA190920WEBGIT/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA190920WEBGIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Szia, hogy hívnak?");
+            var n = Console.ReadLine();
+            Console.WriteLine($"Ó, milyen szép név azm hogy {n}!");
+            Console.WriteLine($"Szerecc programozni {n}? :3");
+            var v = Console.ReadLine();
+            if(v.ToLower() == "igen")
+            {
+                Console.WriteLine("Jaj de jó, akkor mi nagtyon jó barátok leszünk!");
+            }
+            else
+            {
+                Console.WriteLine("Hát akkor bizony kabbe!");
+            }
+            //TODO: csijjag
+            Console.ReadKey();
+        }
+    }
+}
